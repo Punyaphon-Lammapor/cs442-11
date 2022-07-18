ffdf3a5546a1a65ad08e9fb0ae63837497157b78
Punyaphon <punyaphon.l@ku.th>
2022-07-18 11:08:14 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+            //Closure
+Route::get('/about', function () {
+    return "About Me";
+});
+            //?=optional
+Route::get('/pages/{id?}', function ($id=1) {
+    return "Page {$id}";
+});
